c37e8b566703bdc3618d5d03cd26955a09166be9
Wesley Barbosa da Silva <124535880+wBarbosad@users.noreply.github.com>
2023-03-27 18:41:47 -0300

    Update Exercicio1_2.js

--- Lista1/Exercicio1_2.js
+++ Lista1/Exercicio1_2.js
@@ -9,12 +9,12 @@ var idade4 = 40
 var nome5 = "Wesley"
 var idade5 = 15
 
-
-console.log("\n", nome1, " - ", idade1)
-console.log(nome2, " - ", idade2)
-console.log(nome3, " - ", idade3)
-console.log(nome4, " - ", idade4)
-console.log(nome5, " - ", idade5)
+console.log("\n")
+console.log(nome1, "-", idade1)
+console.log(nome2, "-", idade2)
+console.log(nome3, "-", idade3)
+console.log(nome4, "-", idade4)
+console.log(nome5, "-", idade5)
 console.log("\n")
 
 

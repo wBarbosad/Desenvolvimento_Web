83bfcf3085463ac646f42e8722966a16a2b201e2
Wesley Barbosa da Silva <124535880+wBarbosad@users.noreply.github.com>
2023-04-03 18:05:21 -0300

    Lista2 da aula 2

---
+++ Listas/Lista2_2/atvidade1.js
@@ -0,0 +1,11 @@
+var salarioTrabalhador1 = 2500, salarioTrabalhador2 = 3000, salarioTrabalhador3 = 4500
+var salarioTrabalhador4 = 6000, salarioTrabalhador5 = 3000
+var reajuste = (110/100)
+var SalarioReajusteTrabalhador1, SalarioReajusteTrabalhador2, SalarioReajusteTrabalhador3
+var SalarioReajusteTrabalhador4, salarioTrabalhador5
+
+console.log("\nTrabalhador 01: \nSalário atual = R$", salarioTrabalhador1, "\tPassa a receber = R$", (salarioTrabalhador1*reajuste))
+console.log("\nTrabalhador 02: \nSalário atual = R$", salarioTrabalhador2, "\tPassa a receber = R$", (salarioTrabalhador2*reajuste))
+console.log("\nTrabalhador 03: \nSalário atual = R$", salarioTrabalhador3, "\tPassa a receber = R$", (salarioTrabalhador3*reajuste))
+console.log("\nTrabalhador 04: \nSalário atual = R$", salarioTrabalhador4, "\tPassa a receber = R$", (salarioTrabalhador4*reajuste))
+console.log("\nTrabalhador 05: \nSalário atual = R$", salarioTrabalhador5, "\tPassa a receber = R$", (salarioTrabalhador5*reajuste))

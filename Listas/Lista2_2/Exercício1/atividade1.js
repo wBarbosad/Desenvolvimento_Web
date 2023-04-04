91187b18564e071d7f9b85984a5fbf523f778e81
Wesley Barbosa da Silva <124535880+wBarbosad@users.noreply.github.com>
2023-04-03 21:58:59 -0300

    Modificacao exercicios

---
+++ Listas/Lista2_2/Exercício1/atividade1.js
@@ -0,0 +1,14 @@
+var salarioTrabalhador1 = 2500, salarioTrabalhador2 = 3000, salarioTrabalhador3 = 4500
+var salarioTrabalhador4 = 6000, salarioTrabalhador5 = 3000
+var reajuste = (110/100)
+var valorReajuste1 = salarioTrabalhador1 * reajuste
+var valorReajuste2 = salarioTrabalhador2 * reajuste
+var valorReajuste3 = salarioTrabalhador3 * reajuste
+var valorReajuste4 = salarioTrabalhador4 * reajuste
+var valorReajuste5 = salarioTrabalhador5 * reajuste
+
+console.log("\nTrabalhador 01: \nSalário atual = R$", salarioTrabalhador1, "\tPassa a receber = R$", valorReajuste1.toFixed(2))
+console.log("\nTrabalhador 02: \nSalário atual = R$", salarioTrabalhador2, "\tPassa a receber = R$", valorReajuste2.toFixed(2))
+console.log("\nTrabalhador 03: \nSalário atual = R$", salarioTrabalhador3, "\tPassa a receber = R$", valorReajuste3.toFixed(2))
+console.log("\nTrabalhador 04: \nSalário atual = R$", salarioTrabalhador4, "\tPassa a receber = R$", valorReajuste4.toFixed(2))
+console.log("\nTrabalhador 05: \nSalário atual = R$", salarioTrabalhador5, "\tPassa a receber = R$", valorReajuste5.toFixed(2))
